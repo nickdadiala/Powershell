f988afb1a8c8302ba12e238b3b44f87b5d3f4a86
nickdadiala <nick2771910@gmail.com>
2025-07-18 09:23:44 +1200

    setpw lenovo

--- LenovoSetBIOSPassword.ps1
+++ LenovoSetBIOSPassword.ps1
@@ -1,4 +1,4 @@
-# Checks before you run this script
+# Checks before you run this script and confirm Password state is "0"
 Get-CimInstance -Namespace root/WMI -ClassName Lenovo_BiosPasswordSettings
 #Run the commands to set initial Supervisor Password and replace it
 $setPW = Get-WmiObject -Namespace root/WMI -class Lenovo_SetBiosPassword
@@ -9,8 +9,17 @@ Function New-LenovoSetBIOSPassword
 {
    param(
         [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$Namespace,
-        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$ClassName   
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()][string]$ClassName,
+        [Parameter(Mandatory=$true)][ValidateNotNull()][securestring]$password,
+        [Parameter(Mandatory=$false)][ValidateNotNullOrEmpty()][securestring]$setPW
         )
 }
-
-       
+ If($setPW)
+  {
+    $bytes= $Script:Encoder.GetBytes($setPW)
+    if(($securityInterface.SetBioPassword(1,$setPW.length,$bytes,$password)).status -eq 0)
+  {
+    Write-output -Value "$password.Password is set "
+  }
+     
+  }     
