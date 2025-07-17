31fdfe3be09b8a66fa6138c945a74b8536625d18
nickdadiala <nick2771910@gmail.com>
2025-07-18 09:53:13 +1200

    Update patchTPM

--- LenovoSetBIOSPassword.ps1
+++ LenovoSetBIOSPassword.ps1
@@ -4,7 +4,16 @@ Get-CimInstance -Namespace root/WMI -ClassName Lenovo_BiosPasswordSettings
 $setPW = Get-WmiObject -Namespace root/WMI -class Lenovo_SetBiosPassword
 #
 $setPW.SetBioPassword ("pap,secretpassword,secretpassword,ascii,us")
-
+# Chekc the status TPM physical presence
+$tpm = Get-Tpm
+ Write-Host "TPM presence: $($tpm.Tpmpresence)"
+ Write-Host "Tpm Enabled: $($tpm.TpmEnabled)"
+ Write-host "Tpm Disabled: $($tpm.TpmDisabled)"
+  If($tpm.Tpmpresence -and -not $tpm.TpmDisbaled){
+    Write-Host "tpm is present and need to cleared"
+  }else{
+    Write-Host "Tpm is not presnt and can not be cleared"
+  }
 Function New-LenovoSetBIOSPassword 
 {
    param(
@@ -17,9 +26,21 @@ Function New-LenovoSetBIOSPassword
  If($setPW)
   {
     $bytes= $Script:Encoder.GetBytes($setPW)
-    if(($securityInterface.SetBioPassword(1,$setPW.length,$bytes,$password)).status -eq 0)
+    if(($securityInterface.SetBiosPassword(1,$setPW.length,$bytes,$password)).status -eq 0)
   {
-    Write-output -Value "$password.Password is set "
+    Write-output -Value "$password.Password is set"
   }
-     
-  }     
+    else{
+        Write-Output -Name "($password.password exist)" -value "falied"
+    } 
+  }  
+  #set new BIOS PAssword   
+   else {
+    if(($securityInterface.SetBiosPassword(0,0,0, $password)).status -eq 0)
+    {
+        Write-Output -Value "$password.Password is set"
+      else{
+        Write-Output -Name "($password.password exist)" -value "failed"
+      }
+    }
+   }
